48822a348be55fe201208f0e2690aa3c0f0a499c
Colin Richardson <colinr@mbmsoftwareltd.co.uk>
2019-11-02 19:37:41 +0000

    Added list endpoint and test

--- get.js
+++ get.js
@@ -8,8 +8,7 @@ export async function main(event, context) {
         // - 'userId': Identity Pool identity id of the authenticated user
         // - 'noteId': path parameter
         Key: {
-            userId:
-            event.requestContext.identity.cognitoIdentityId,
+            userId: event.requestContext.identity.cognitoIdentityId,
             noteId: event.pathParameters.id
         }
     };
